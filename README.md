57477db8373154f6c43a2bb84660874a11522d3a
AhnSinYong <ahnsy29@gmail.com>
2022-05-30 18:13:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-q6yxgs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-q6yxgs)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { products } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  constructor() {}
+
+  items = [];
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
